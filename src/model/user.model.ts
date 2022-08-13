ab55c182defb906cc67a29311fc187b4612e72dc
ced <iarecaringpeople@gmail.com>
2022-08-13 22:01:05 +0800

    .

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -4,10 +4,20 @@ import {
   prop,
   Severity,
   pre,
+  DocumentType,
+  index,
 } from "@typegoose/typegoose";
 import { nanoid } from "nanoid";
 import argon2 from "argon2";
 import log from "../utils/logger";
+
+export const privateFields = [
+  "password",
+  "__v",
+  "verificationCode",
+  "passwordResetCode",
+  "verified",
+];
 
 @pre<User>("save", async function () {
   if (!this.isModified("password")) {
@@ -20,6 +30,7 @@ import log from "../utils/logger";
 
   return;
 })
+@index({ email: 1 })
 @modelOptions({
   schemaOptions: {
     timestamps: true,
